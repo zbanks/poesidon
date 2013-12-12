e12dd875489fe1e7ed9b6fad4ee4ab86a4540894
Zach Banks <zbanks@mit.edu>
2013-12-12 17:17:49 -0500

    Fixed images.c script

--- images.h
+++ images.h
@@ -1,30 +1,31 @@
-#include <stdint.h>// File: 'images/splash.png'
-#define IMAGES/SPLASH_IMAGE IMAGES/SPLASH_WIDTH,IMAGES/SPLASH_HEIGHT,(uint8_t*)IMAGES/SPLASH_DATA
-extern const uint8_t IMAGES/SPLASH_WIDTH;
-extern const uint8_t IMAGES/SPLASH_HEIGHT;
-extern const uint8_t IMAGES/SPLASH_DATA[16900];
+#include <stdint.h>
+// File: 'images/splash.png'
+#define SPLASH_IMAGE SPLASH_WIDTH,SPLASH_HEIGHT,(uint8_t*)SPLASH_DATA
+extern const uint8_t SPLASH_WIDTH;
+extern const uint8_t SPLASH_HEIGHT;
+extern const uint8_t SPLASH_DATA[16900];
 
 // File: 'images/txt_bright_laser.png'
-#define IMAGES/TXT_BRIGHT_LASER_IMAGE IMAGES/TXT_BRIGHT_LASER_WIDTH,IMAGES/TXT_BRIGHT_LASER_HEIGHT,(uint8_t*)IMAGES/TXT_BRIGHT_LASER_DATA
-extern const uint8_t IMAGES/TXT_BRIGHT_LASER_WIDTH;
-extern const uint8_t IMAGES/TXT_BRIGHT_LASER_HEIGHT;
-extern const uint8_t IMAGES/TXT_BRIGHT_LASER_DATA[1139];
+#define TXT_BRIGHT_LASER_IMAGE TXT_BRIGHT_LASER_WIDTH,TXT_BRIGHT_LASER_HEIGHT,(uint8_t*)TXT_BRIGHT_LASER_DATA
+extern const uint8_t TXT_BRIGHT_LASER_WIDTH;
+extern const uint8_t TXT_BRIGHT_LASER_HEIGHT;
+extern const uint8_t TXT_BRIGHT_LASER_DATA[1139];
 
 // File: 'images/txt_much_length.png'
-#define IMAGES/TXT_MUCH_LENGTH_IMAGE IMAGES/TXT_MUCH_LENGTH_WIDTH,IMAGES/TXT_MUCH_LENGTH_HEIGHT,(uint8_t*)IMAGES/TXT_MUCH_LENGTH_DATA
-extern const uint8_t IMAGES/TXT_MUCH_LENGTH_WIDTH;
-extern const uint8_t IMAGES/TXT_MUCH_LENGTH_HEIGHT;
-extern const uint8_t IMAGES/TXT_MUCH_LENGTH_DATA[1105];
+#define TXT_MUCH_LENGTH_IMAGE TXT_MUCH_LENGTH_WIDTH,TXT_MUCH_LENGTH_HEIGHT,(uint8_t*)TXT_MUCH_LENGTH_DATA
+extern const uint8_t TXT_MUCH_LENGTH_WIDTH;
+extern const uint8_t TXT_MUCH_LENGTH_HEIGHT;
+extern const uint8_t TXT_MUCH_LENGTH_DATA[1105];
 
 // File: 'images/txt_such_speed.png'
-#define IMAGES/TXT_SUCH_SPEED_IMAGE IMAGES/TXT_SUCH_SPEED_WIDTH,IMAGES/TXT_SUCH_SPEED_HEIGHT,(uint8_t*)IMAGES/TXT_SUCH_SPEED_DATA
-extern const uint8_t IMAGES/TXT_SUCH_SPEED_WIDTH;
-extern const uint8_t IMAGES/TXT_SUCH_SPEED_HEIGHT;
-extern const uint8_t IMAGES/TXT_SUCH_SPEED_DATA[986];
+#define TXT_SUCH_SPEED_IMAGE TXT_SUCH_SPEED_WIDTH,TXT_SUCH_SPEED_HEIGHT,(uint8_t*)TXT_SUCH_SPEED_DATA
+extern const uint8_t TXT_SUCH_SPEED_WIDTH;
+extern const uint8_t TXT_SUCH_SPEED_HEIGHT;
+extern const uint8_t TXT_SUCH_SPEED_DATA[986];
 
 // File: 'images/txt_wow.png'
-#define IMAGES/TXT_WOW_IMAGE IMAGES/TXT_WOW_WIDTH,IMAGES/TXT_WOW_HEIGHT,(uint8_t*)IMAGES/TXT_WOW_DATA
-extern const uint8_t IMAGES/TXT_WOW_WIDTH;
-extern const uint8_t IMAGES/TXT_WOW_HEIGHT;
-extern const uint8_t IMAGES/TXT_WOW_DATA[374];
+#define TXT_WOW_IMAGE TXT_WOW_WIDTH,TXT_WOW_HEIGHT,(uint8_t*)TXT_WOW_DATA
+extern const uint8_t TXT_WOW_WIDTH;
+extern const uint8_t TXT_WOW_HEIGHT;
+extern const uint8_t TXT_WOW_DATA[374];
 
--- python_scripts/imgconv.py
+++ python_scripts/imgconv.py
@@ -9,8 +9,8 @@ def condense(r,g,b,a=0):
 
 with open("images.h", "w") as fh:
     with open("images.c", "w") as fc:
-        fc.write("#include <images.h>")
-        fh.write("#include <stdint.h>")
+        fc.write("#include <images.h>\n")
+        fh.write("#include <stdint.h>\n")
         for fname in sys.argv[1:]:
             fh.write("// File: '{}'\n".format(fname))
             fc.write("// File: '{}'\n".format(fname))
@@ -18,7 +18,7 @@ with open("images.h", "w") as fh:
             im=Image.open(fname)
             im=im.rotate(-90)
             pixels=','.join(['0x{0:X}'.format(condense(*pix)) for pix in im.getdata()])
-            varname = fname.split('.')[0].upper()
+            varname = fname.split('.')[0].split("/")[-1].upper()
 
             fc.write("const uint8_t {0}_WIDTH = {1};\n".format(varname,im.size[0]))
             fc.write("const uint8_t {0}_HEIGHT = {1};\n".format(varname,im.size[1]))
