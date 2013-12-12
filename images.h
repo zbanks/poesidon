d511228534da17497aa00d82fb9e2e8ed7d23c23
Zach Banks <zbanks@mit.edu>
2013-12-11 22:19:32 -0500

    Re-wrote imgconf.py to generate images.{c,h} files

---
+++ images.h
@@ -0,0 +1,15 @@
+// File: 'Poseidon_doge_1.png'
+extern const uint8_t POSEIDON_DOGE_1_WIDTH;
+extern const uint8_t POSEIDON_DOGE_1_HEIGHT;
+extern const uint8_t POSEIDON_DOGE_1_DATA[16900];
+
+// File: 'Poseidon_doge_2.png'
+extern const uint8_t POSEIDON_DOGE_2_WIDTH;
+extern const uint8_t POSEIDON_DOGE_2_HEIGHT;
+extern const uint8_t POSEIDON_DOGE_2_DATA[16900];
+
+// File: 'T-payne_doge.png'
+extern const uint8_t T-PAYNE_DOGE_WIDTH;
+extern const uint8_t T-PAYNE_DOGE_HEIGHT;
+extern const uint8_t T-PAYNE_DOGE_DATA[16900];
+
--- imgconv.py
+++ imgconv.py
@@ -7,11 +7,23 @@ def condense(r,g,b,a=0):
 	b=b>>6
 	return r<<5 | g<<2 | b
 
-f=sys.argv[1]
-im=Image.open(f)
-im=im.rotate(-90)
-pixels=','.join(['0x{0:X}'.format(condense(*pix)) for pix in im.getdata()])
-varname=sys.argv[1].split('.')[0].upper()
-print "const uint8_t {0}_WIDTH = {1};".format(varname,im.size[0])
-print "const uint8_t {0}_HEIGHT = {1};".format(varname,im.size[1])
-print "const uint8_t {0}_DATA[{1}] = {{{2}}};".format(varname,im.size[0]*im.size[1],pixels)
+with open("images.h", "w") as fh:
+    with open("images.c", "w") as fc:
+        for fname in sys.argv[1:]:
+            fh.write("// File: '{}'\n".format(fname))
+            fc.write("// File: '{}'\n".format(fname))
+
+            im=Image.open(fname)
+            im=im.rotate(-90)
+            pixels=','.join(['0x{0:X}'.format(condense(*pix)) for pix in im.getdata()])
+            varname = fname.split('.')[0].upper()
+
+            fc.write("const uint8_t {0}_WIDTH = {1};\n".format(varname,im.size[0]))
+            fc.write("const uint8_t {0}_HEIGHT = {1};\n".format(varname,im.size[1]))
+            fc.write("const uint8_t {0}_DATA[{1}] = {{{2}}};\n".format(varname,im.size[0]*im.size[1],pixels))
+            fc.write("\n")
+
+            fh.write("extern const uint8_t {0}_WIDTH;\n".format(varname))
+            fh.write("extern const uint8_t {0}_HEIGHT;\n".format(varname))
+            fh.write("extern const uint8_t {0}_DATA[{1}];\n".format(varname,im.size[0]*im.size[1]))
+            fh.write("\n")
