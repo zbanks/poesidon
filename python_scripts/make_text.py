b2dcde7af9f21ec7d56b4e2d7116d1ba2c482463
Zach Banks <zbanks@mit.edu>
2013-12-12 17:10:49 -0500

    Text images

--- python_scripts/make_text.py
+++ python_scripts/make_text.py
@@ -1,10 +1,11 @@
 from PIL import Image,ImageDraw,ImageFont
 import sys
 
-txt=sys.argv[1]
+txt=sys.argv[3]
 
-fontsize=14
+fontsize=int(sys.argv[1])
 
+#font = ImageFont.truetype("/usr/share/fonts/truetype/msttcorefonts/Comic_Sans_MS_Bold.ttf", fontsize)
 font = ImageFont.truetype("/usr/share/fonts/truetype/msttcorefonts/Comic_Sans_MS.ttf", fontsize)
 draw=ImageDraw.Draw(Image.new("RGB",(1,1)))
 ts=draw.textsize(txt,font)
@@ -14,4 +15,5 @@ draw = ImageDraw.Draw(image)
 
 draw.text((0, 0), txt, (0,0,0), font=font)
 
-image.show()
+#image.show()
+image.save(sys.argv[2])
