da5460dfcc707cc178a775c2472d5c3ce46d378d
Zach Banks <zbanks@mit.edu>
2013-12-13 18:48:34 -0500

    I guess not every images should be on the SD card

--- images.h
+++ images.h
@@ -1,164 +1,164 @@
 #include <stdint.h>
 
-// Sprite File: 'images/circle.spng'
+// Bitmap File: 'images/circle.spng'
 #define CIRCLE_IMAGE CIRCLE_WIDTH,CIRCLE_HEIGHT,(uint8_t*)CIRCLE_DATA
 #define CIRCLE_WIDTH (40)
 #define CIRCLE_HEIGHT (40)
-extern const uint8_t CIRCLE_DATA[800];
+extern const uint8_t CIRCLE_DATA[1600];
 
 // Bitmap File: 'images/doge_water.png'
-#define DOGE_WATER_IMAGE DOGE_WATER_WIDTH,DOGE_WATER_HEIGHT,(uint8_t*)DOGE_WATER_OFFSET
+#define DOGE_WATER_IMAGE DOGE_WATER_WIDTH,DOGE_WATER_HEIGHT,(uint8_t*)DOGE_WATER_DATA
 #define DOGE_WATER_WIDTH (130)
 #define DOGE_WATER_HEIGHT (130)
-#define DOGE_WATER_OFFSET (0x0)
+extern const uint8_t DOGE_WATER_DATA[16900];
 
-// Sprite File: 'images/dot.spng'
+// Bitmap File: 'images/dot.spng'
 #define DOT_IMAGE DOT_WIDTH,DOT_HEIGHT,(uint8_t*)DOT_DATA
 #define DOT_WIDTH (40)
 #define DOT_HEIGHT (40)
-extern const uint8_t DOT_DATA[800];
+extern const uint8_t DOT_DATA[1600];
 
-// Sprite File: 'images/line.spng'
+// Bitmap File: 'images/line.spng'
 #define LINE_IMAGE LINE_WIDTH,LINE_HEIGHT,(uint8_t*)LINE_DATA
 #define LINE_WIDTH (40)
 #define LINE_HEIGHT (80)
-extern const uint8_t LINE_DATA[1600];
+extern const uint8_t LINE_DATA[3200];
 
 // Bitmap File: 'images/poesidon_doge_1.png'
-#define POESIDON_DOGE_1_IMAGE POESIDON_DOGE_1_WIDTH,POESIDON_DOGE_1_HEIGHT,(uint8_t*)POESIDON_DOGE_1_OFFSET
+#define POESIDON_DOGE_1_IMAGE POESIDON_DOGE_1_WIDTH,POESIDON_DOGE_1_HEIGHT,(uint8_t*)POESIDON_DOGE_1_DATA
 #define POESIDON_DOGE_1_WIDTH (130)
 #define POESIDON_DOGE_1_HEIGHT (130)
-#define POESIDON_DOGE_1_OFFSET (0x4400)
+extern const uint8_t POESIDON_DOGE_1_DATA[16900];
 
 // Bitmap File: 'images/splash.png'
-#define SPLASH_IMAGE SPLASH_WIDTH,SPLASH_HEIGHT,(uint8_t*)SPLASH_OFFSET
+#define SPLASH_IMAGE SPLASH_WIDTH,SPLASH_HEIGHT,(uint8_t*)SPLASH_DATA
 #define SPLASH_WIDTH (130)
 #define SPLASH_HEIGHT (130)
-#define SPLASH_OFFSET (0x8800)
+extern const uint8_t SPLASH_DATA[16900];
 
-// Sprite File: 'images/splash_txt.spng'
+// Bitmap File: 'images/splash_txt.spng'
 #define SPLASH_TXT_IMAGE SPLASH_TXT_WIDTH,SPLASH_TXT_HEIGHT,(uint8_t*)SPLASH_TXT_DATA
 #define SPLASH_TXT_WIDTH (65)
 #define SPLASH_TXT_HEIGHT (101)
-extern const uint8_t SPLASH_TXT_DATA[3283];
+extern const uint8_t SPLASH_TXT_DATA[6565];
 
-// Sprite File: 'images/square.spng'
+// Bitmap File: 'images/square.spng'
 #define SQUARE_IMAGE SQUARE_WIDTH,SQUARE_HEIGHT,(uint8_t*)SQUARE_DATA
 #define SQUARE_WIDTH (40)
 #define SQUARE_HEIGHT (40)
-extern const uint8_t SQUARE_DATA[800];
+extern const uint8_t SQUARE_DATA[1600];
 
 // Bitmap File: 'images/swimming.png'
-#define SWIMMING_IMAGE SWIMMING_WIDTH,SWIMMING_HEIGHT,(uint8_t*)SWIMMING_OFFSET
+#define SWIMMING_IMAGE SWIMMING_WIDTH,SWIMMING_HEIGHT,(uint8_t*)SWIMMING_DATA
 #define SWIMMING_WIDTH (130)
 #define SWIMMING_HEIGHT (130)
-#define SWIMMING_OFFSET (0xcc00)
+extern const uint8_t SWIMMING_DATA[16900];
 
 // Bitmap File: 'images/tpayne.png'
-#define TPAYNE_IMAGE TPAYNE_WIDTH,TPAYNE_HEIGHT,(uint8_t*)TPAYNE_OFFSET
+#define TPAYNE_IMAGE TPAYNE_WIDTH,TPAYNE_HEIGHT,(uint8_t*)TPAYNE_DATA
 #define TPAYNE_WIDTH (130)
 #define TPAYNE_HEIGHT (130)
-#define TPAYNE_OFFSET (0x11000)
+extern const uint8_t TPAYNE_DATA[16900];
 
-// Sprite File: 'images/txt_0.spng'
+// Bitmap File: 'images/txt_0.spng'
 #define TXT_0_IMAGE TXT_0_WIDTH,TXT_0_HEIGHT,(uint8_t*)TXT_0_DATA
 #define TXT_0_WIDTH (56)
 #define TXT_0_HEIGHT (24)
-extern const uint8_t TXT_0_DATA[672];
+extern const uint8_t TXT_0_DATA[1344];
 
-// Sprite File: 'images/txt_1.spng'
+// Bitmap File: 'images/txt_1.spng'
 #define TXT_1_IMAGE TXT_1_WIDTH,TXT_1_HEIGHT,(uint8_t*)TXT_1_DATA
 #define TXT_1_WIDTH (56)
 #define TXT_1_HEIGHT (24)
-extern const uint8_t TXT_1_DATA[672];
+extern const uint8_t TXT_1_DATA[1344];
 
-// Sprite File: 'images/txt_2.spng'
+// Bitmap File: 'images/txt_2.spng'
 #define TXT_2_IMAGE TXT_2_WIDTH,TXT_2_HEIGHT,(uint8_t*)TXT_2_DATA
 #define TXT_2_WIDTH (56)
 #define TXT_2_HEIGHT (24)
-extern const uint8_t TXT_2_DATA[672];
+extern const uint8_t TXT_2_DATA[1344];
 
-// Sprite File: 'images/txt_3.spng'
+// Bitmap File: 'images/txt_3.spng'
 #define TXT_3_IMAGE TXT_3_WIDTH,TXT_3_HEIGHT,(uint8_t*)TXT_3_DATA
 #define TXT_3_WIDTH (56)
 #define TXT_3_HEIGHT (24)
-extern const uint8_t TXT_3_DATA[672];
+extern const uint8_t TXT_3_DATA[1344];
 
-// Sprite File: 'images/txt_4.spng'
+// Bitmap File: 'images/txt_4.spng'
 #define TXT_4_IMAGE TXT_4_WIDTH,TXT_4_HEIGHT,(uint8_t*)TXT_4_DATA
 #define TXT_4_WIDTH (56)
 #define TXT_4_HEIGHT (24)
-extern const uint8_t TXT_4_DATA[672];
+extern const uint8_t TXT_4_DATA[1344];
 
-// Sprite File: 'images/txt_5.spng'
+// Bitmap File: 'images/txt_5.spng'
 #define TXT_5_IMAGE TXT_5_WIDTH,TXT_5_HEIGHT,(uint8_t*)TXT_5_DATA
 #define TXT_5_WIDTH (56)
 #define TXT_5_HEIGHT (24)
-extern const uint8_t TXT_5_DATA[672];
+extern const uint8_t TXT_5_DATA[1344];
 
-// Sprite File: 'images/txt_6.spng'
+// Bitmap File: 'images/txt_6.spng'
 #define TXT_6_IMAGE TXT_6_WIDTH,TXT_6_HEIGHT,(uint8_t*)TXT_6_DATA
 #define TXT_6_WIDTH (56)
 #define TXT_6_HEIGHT (24)
-extern const uint8_t TXT_6_DATA[672];
+extern const uint8_t TXT_6_DATA[1344];
 
-// Sprite File: 'images/txt_7.spng'
+// Bitmap File: 'images/txt_7.spng'
 #define TXT_7_IMAGE TXT_7_WIDTH,TXT_7_HEIGHT,(uint8_t*)TXT_7_DATA
 #define TXT_7_WIDTH (56)
 #define TXT_7_HEIGHT (24)
-extern const uint8_t TXT_7_DATA[672];
+extern const uint8_t TXT_7_DATA[1344];
 
-// Sprite File: 'images/txt_8.spng'
+// Bitmap File: 'images/txt_8.spng'
 #define TXT_8_IMAGE TXT_8_WIDTH,TXT_8_HEIGHT,(uint8_t*)TXT_8_DATA
 #define TXT_8_WIDTH (56)
 #define TXT_8_HEIGHT (24)
-extern const uint8_t TXT_8_DATA[672];
+extern const uint8_t TXT_8_DATA[1344];
 
-// Sprite File: 'images/txt_9.spng'
+// Bitmap File: 'images/txt_9.spng'
 #define TXT_9_IMAGE TXT_9_WIDTH,TXT_9_HEIGHT,(uint8_t*)TXT_9_DATA
 #define TXT_9_WIDTH (56)
 #define TXT_9_HEIGHT (24)
-extern const uint8_t TXT_9_DATA[672];
+extern const uint8_t TXT_9_DATA[1344];
 
-// Sprite File: 'images/txt_bright_laser.spng'
+// Bitmap File: 'images/txt_bright_laser.spng'
 #define TXT_BRIGHT_LASER_IMAGE TXT_BRIGHT_LASER_WIDTH,TXT_BRIGHT_LASER_HEIGHT,(uint8_t*)TXT_BRIGHT_LASER_DATA
 #define TXT_BRIGHT_LASER_WIDTH (18)
 #define TXT_BRIGHT_LASER_HEIGHT (75)
-extern const uint8_t TXT_BRIGHT_LASER_DATA[675];
+extern const uint8_t TXT_BRIGHT_LASER_DATA[1350];
 
-// Sprite File: 'images/txt_how_many_feat_long.spng'
+// Bitmap File: 'images/txt_how_many_feat_long.spng'
 #define TXT_HOW_MANY_FEAT_LONG_IMAGE TXT_HOW_MANY_FEAT_LONG_WIDTH,TXT_HOW_MANY_FEAT_LONG_HEIGHT,(uint8_t*)TXT_HOW_MANY_FEAT_LONG_DATA
 #define TXT_HOW_MANY_FEAT_LONG_WIDTH (17)
 #define TXT_HOW_MANY_FEAT_LONG_HEIGHT (119)
-extern const uint8_t TXT_HOW_MANY_FEAT_LONG_DATA[1012];
+extern const uint8_t TXT_HOW_MANY_FEAT_LONG_DATA[2023];
 
-// Sprite File: 'images/txt_much_deep.spng'
+// Bitmap File: 'images/txt_much_deep.spng'
 #define TXT_MUCH_DEEP_IMAGE TXT_MUCH_DEEP_WIDTH,TXT_MUCH_DEEP_HEIGHT,(uint8_t*)TXT_MUCH_DEEP_DATA
 #define TXT_MUCH_DEEP_WIDTH (25)
 #define TXT_MUCH_DEEP_HEIGHT (91)
-extern const uint8_t TXT_MUCH_DEEP_DATA[1138];
+extern const uint8_t TXT_MUCH_DEEP_DATA[2275];
 
-// Sprite File: 'images/txt_much_length.spng'
+// Bitmap File: 'images/txt_much_length.spng'
 #define TXT_MUCH_LENGTH_IMAGE TXT_MUCH_LENGTH_WIDTH,TXT_MUCH_LENGTH_HEIGHT,(uint8_t*)TXT_MUCH_LENGTH_DATA
 #define TXT_MUCH_LENGTH_WIDTH (18)
 #define TXT_MUCH_LENGTH_HEIGHT (77)
-extern const uint8_t TXT_MUCH_LENGTH_DATA[693];
+extern const uint8_t TXT_MUCH_LENGTH_DATA[1386];
 
-// Sprite File: 'images/txt_so_shallow.spng'
+// Bitmap File: 'images/txt_so_shallow.spng'
 #define TXT_SO_SHALLOW_IMAGE TXT_SO_SHALLOW_WIDTH,TXT_SO_SHALLOW_HEIGHT,(uint8_t*)TXT_SO_SHALLOW_DATA
 #define TXT_SO_SHALLOW_WIDTH (25)
 #define TXT_SO_SHALLOW_HEIGHT (87)
-extern const uint8_t TXT_SO_SHALLOW_DATA[1088];
+extern const uint8_t TXT_SO_SHALLOW_DATA[2175];
 
-// Sprite File: 'images/txt_such_speed.spng'
+// Bitmap File: 'images/txt_such_speed.spng'
 #define TXT_SUCH_SPEED_IMAGE TXT_SUCH_SPEED_WIDTH,TXT_SUCH_SPEED_HEIGHT,(uint8_t*)TXT_SUCH_SPEED_DATA
 #define TXT_SUCH_SPEED_WIDTH (18)
 #define TXT_SUCH_SPEED_HEIGHT (69)
-extern const uint8_t TXT_SUCH_SPEED_DATA[621];
+extern const uint8_t TXT_SUCH_SPEED_DATA[1242];
 
-// Sprite File: 'images/txt_wow.spng'
+// Bitmap File: 'images/txt_wow.spng'
 #define TXT_WOW_IMAGE TXT_WOW_WIDTH,TXT_WOW_HEIGHT,(uint8_t*)TXT_WOW_DATA
 #define TXT_WOW_WIDTH (18)
 #define TXT_WOW_HEIGHT (25)
-extern const uint8_t TXT_WOW_DATA[225];
+extern const uint8_t TXT_WOW_DATA[450];
 
--- python_scripts/imgconv.py
+++ python_scripts/imgconv.py
@@ -25,10 +25,26 @@ with open("images.h", "w") as fh:
                 im=im.rotate(-90)
 
                 sprites = fname.endswith(".spng")
+                obj = fname.endswith(".opng")
 
-                if not sprites:
+                if not obj or not sprites:
                     fh.write("// Bitmap File: '{}'\n".format(fname))
                     fc.write("// Bitmap File: '{}'\n".format(fname))
+                    pixels=','.join(['0x{0:X}'.format(condense(*pix)) for pix in im.getdata()])
+                    varname = fname.split('.')[0].split("/")[-1].upper()
+                    size = (im.size[0] * im.size[1])
+                    
+                    fc.write("const uint8_t {0}_DATA[{1}] = {{{2}}};\n".format(varname,im.size[0]*im.size[1],pixels))
+                    fc.write("\n")
+
+                    fh.write("#define {0}_IMAGE {0}_WIDTH,{0}_HEIGHT,(uint8_t*){0}_DATA\n".format(varname))
+                    fh.write("#define {0}_WIDTH ({1})\n".format(varname,im.size[0]))
+                    fh.write("#define {0}_HEIGHT ({1})\n".format(varname,im.size[1]))
+                    fh.write("extern const uint8_t {0}_DATA[{1}];\n".format(varname,im.size[0]*im.size[1]))
+                    fh.write("\n")
+                elif obj:
+                    fh.write("// SD Object Bitmap File: '{}'\n".format(fname))
+                    fc.write("// SD Object Bitmap File: '{}'\n".format(fname))
                     pixels=','.join(['0x{0:X}'.format(condense(*pix)) for pix in im.getdata()])
                     varname = fname.split('.')[0].split("/")[-1].upper()
                     size = (im.size[0] * im.size[1])
