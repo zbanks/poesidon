6e8c831a50b7ce93fd0cf5d6c691b47cab7d5f34
Zach Banks <zbanks@mit.edu>
2013-12-12 19:55:57 -0500

    Tweaked images & python

--- python_scripts/make_text.py
+++ python_scripts/make_text.py
@@ -1,19 +1,19 @@
 from PIL import Image,ImageDraw,ImageFont
 import sys
 
-txt=sys.argv[3]
+#txt=sys.argv[3]
 
 fontsize=int(sys.argv[1])
 
-#font = ImageFont.truetype("/usr/share/fonts/truetype/msttcorefonts/Comic_Sans_MS_Bold.ttf", fontsize)
-font = ImageFont.truetype("/usr/share/fonts/truetype/msttcorefonts/Comic_Sans_MS.ttf", fontsize)
-draw=ImageDraw.Draw(Image.new("RGB",(1,1)))
-ts=draw.textsize(txt,font)
+for txt in sys.argv[2:]:
+    font = ImageFont.truetype("/usr/share/fonts/truetype/msttcorefonts/Comic_Sans_MS_Bold.ttf", fontsize)
+#font = ImageFont.truetype("/usr/share/fonts/truetype/msttcorefonts/Comic_Sans_MS.ttf", fontsize)
+    draw=ImageDraw.Draw(Image.new("RGB",(1,1)))
+    ts=draw.textsize(txt,font)
 
-image = Image.new("RGB", ts, (255,255,255))
-draw = ImageDraw.Draw(image)
+    image = Image.new("RGB", ts, (255,255,255))
+    draw = ImageDraw.Draw(image)
 
-draw.text((0, 0), txt, (0,0,0), font=font)
+    draw.text((0, 0), txt, (0,0,0), font=font)
 
-#image.show()
-image.save(sys.argv[2])
+    image.save("txt_{}.png".format(txt.replace(" ", "_")))
