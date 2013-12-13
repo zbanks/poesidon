bf7b1258197d3d6f09664434cb58cbdd3bb3e8a6
Eric Van Albert <ericva1992@gmail.com>
2013-12-13 03:47:28 -0500

    stupid things

--- menus.c
+++ menus.c
@@ -13,6 +13,8 @@ uint8_t RAINBOW[7]={0xE0,0xF4,0xFC,0x1C,0x1F,0x4B,0xE3};
 #define LASER_BG_DATA SPLASH_DATA
 #define WOW_BG_IMAGE SPLASH_IMAGE
 #define WOW_BG_DATA SPLASH_DATA
+#define LENGTH_BG_IMAGE SPLASH_IMAGE
+#define LENGTH_BG_DATA SPLASH_DATA
 
 // The main settings menu
 void menu_main_init();
@@ -93,25 +95,12 @@ inline void draw_sprite_color(enum sprite_names s, const uint8_t* bg, uint8_t co
 
 const uint8_t konami_code[] = {BUTTON_UP, BUTTON_UP, BUTTON_DOWN, BUTTON_DOWN, BUTTON_LEFT, BUTTON_RIGHT, BUTTON_LEFT, BUTTON_RIGHT, BUTTON_B, BUTTON_A};
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-shape_t setting_laser_shape;
-int setting_depth;
-int setting_length;
-int setting_speed;
-=======
-=======
->>>>>>> df599975f4dec2febcb153fc2c02ccbea803d32d
-enum laser_shape setting_laser_shape = CIRCLE;
+shape_t setting_laser_shape = CIRCLE;
 int setting_depth = 0;
 int setting_length = 100;
 int setting_speed = 60;
-<<<<<<< HEAD
->>>>>>> df599975f4dec2febcb153fc2c02ccbea803d32d
 
 int wow_start_time;
-=======
->>>>>>> df599975f4dec2febcb153fc2c02ccbea803d32d
 
 // Begin Functions
 
@@ -267,14 +256,14 @@ void menu_length_run(){
 
     if(length_state == 0){
         if(setting_depth == 0){
-            if(button_edge & (BUTTON_DOWN)){
+            if(buttons_edge & (BUTTON_DOWN)){
                 draw_sprite(TXT_SO_SHALLOW_SPRITE, LENGTH_BG_DATA);
                 setting_depth = 1;
             }else{
                 draw_sprite_color(TXT_SO_SHALLOW_SPRITE, LENGTH_BG_DATA, rainbow_color);
             }
         }else{
-            if(button_edge & (BUTTON_UP)){
+            if(buttons_edge & (BUTTON_UP)){
                 draw_sprite(TXT_MUCH_DEEP_SPRITE, LENGTH_BG_DATA);
                 setting_depth = 0;
             }else{
@@ -283,12 +272,12 @@ void menu_length_run(){
         }
     }else if(length_state == 1){
         // Hundreds Digit
-        if(button_edge & BUTTON_DOWN){
+        if(buttons_edge & BUTTON_DOWN){
             if(setting_length >= 100){
                 setting_length -= 100;
             }
         }
-        if(button_edge & BUTTON_UP){
+        if(buttons_edge & BUTTON_UP){
             if(setting_length < 900){
                 setting_length += 100;
             }
@@ -296,12 +285,12 @@ void menu_length_run(){
         // TODO: Rainbow digit
     }else if(length_state == 2){
         // Tens Digit
-        if(button_edge & BUTTON_DOWN){
+        if(buttons_edge & BUTTON_DOWN){
             if(setting_length % 100 >= 10){
                 setting_length -= 10;
             }
         }
-        if(button_edge & BUTTON_UP){
+        if(buttons_edge & BUTTON_UP){
             if(setting_length % 100 < 90){
                 setting_length += 10;
             }
@@ -309,12 +298,12 @@ void menu_length_run(){
         // TODO: Rainbow digit
     }else if(length_state == 3){
         // Tens Digit
-        if(button_edge & BUTTON_DOWN){
+        if(buttons_edge & BUTTON_DOWN){
             if(setting_length % 10 >= 1){
                 setting_length -= 1;
             }
         }
-        if(button_edge & BUTTON_UP){
+        if(buttons_edge & BUTTON_UP){
             if(setting_length % 10 < 900){
                 setting_length += 1;
             }
@@ -322,13 +311,13 @@ void menu_length_run(){
         // TODO: Rainbow digit
     }
 
-    if(button_edge & (BUTTON_A | BUTTON_LEFT)){
+    if(buttons_edge & (BUTTON_A | BUTTON_LEFT)){
         length_state = (length_state + 1) % 4;
     }
-    if((button_edge & BUTTON_RIGHT) && length_state){
+    if((buttons_edge & BUTTON_RIGHT) && length_state){
         length_state--;
     }
-    if(button_edge & BUTTON_B){
+    if(buttons_edge & BUTTON_B){
         length_state = 0;
         state = &menu_main;
     }
