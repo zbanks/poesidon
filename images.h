ee14879f5c56bf2564b044aa634bc6eb860e9be2
Zach Banks <zbanks@mit.edu>
2013-12-12 19:11:21 -0500

    Changed imgconv.py to use macros for WIDTH & HEIGHT

--- images.h
+++ images.h
@@ -1,31 +1,31 @@
 #include <stdint.h>
 // File: 'images/splash.png'
 #define SPLASH_IMAGE SPLASH_WIDTH,SPLASH_HEIGHT,(uint8_t*)SPLASH_DATA
-extern const uint8_t SPLASH_WIDTH;
-extern const uint8_t SPLASH_HEIGHT;
+#define SPLASH_WIDTH (130)
+#define SPLASH_HEIGHT (130)
 extern const uint8_t SPLASH_DATA[16900];
 
 // File: 'images/txt_bright_laser.png'
 #define TXT_BRIGHT_LASER_IMAGE TXT_BRIGHT_LASER_WIDTH,TXT_BRIGHT_LASER_HEIGHT,(uint8_t*)TXT_BRIGHT_LASER_DATA
-extern const uint8_t TXT_BRIGHT_LASER_WIDTH;
-extern const uint8_t TXT_BRIGHT_LASER_HEIGHT;
+#define TXT_BRIGHT_LASER_WIDTH (17)
+#define TXT_BRIGHT_LASER_HEIGHT (67)
 extern const uint8_t TXT_BRIGHT_LASER_DATA[1139];
 
 // File: 'images/txt_much_length.png'
 #define TXT_MUCH_LENGTH_IMAGE TXT_MUCH_LENGTH_WIDTH,TXT_MUCH_LENGTH_HEIGHT,(uint8_t*)TXT_MUCH_LENGTH_DATA
-extern const uint8_t TXT_MUCH_LENGTH_WIDTH;
-extern const uint8_t TXT_MUCH_LENGTH_HEIGHT;
+#define TXT_MUCH_LENGTH_WIDTH (17)
+#define TXT_MUCH_LENGTH_HEIGHT (65)
 extern const uint8_t TXT_MUCH_LENGTH_DATA[1105];
 
 // File: 'images/txt_such_speed.png'
 #define TXT_SUCH_SPEED_IMAGE TXT_SUCH_SPEED_WIDTH,TXT_SUCH_SPEED_HEIGHT,(uint8_t*)TXT_SUCH_SPEED_DATA
-extern const uint8_t TXT_SUCH_SPEED_WIDTH;
-extern const uint8_t TXT_SUCH_SPEED_HEIGHT;
+#define TXT_SUCH_SPEED_WIDTH (17)
+#define TXT_SUCH_SPEED_HEIGHT (58)
 extern const uint8_t TXT_SUCH_SPEED_DATA[986];
 
 // File: 'images/txt_wow.png'
 #define TXT_WOW_IMAGE TXT_WOW_WIDTH,TXT_WOW_HEIGHT,(uint8_t*)TXT_WOW_DATA
-extern const uint8_t TXT_WOW_WIDTH;
-extern const uint8_t TXT_WOW_HEIGHT;
+#define TXT_WOW_WIDTH (17)
+#define TXT_WOW_HEIGHT (22)
 extern const uint8_t TXT_WOW_DATA[374];
 
--- python_scripts/imgconv.py
+++ python_scripts/imgconv.py
@@ -20,13 +20,11 @@ with open("images.h", "w") as fh:
             pixels=','.join(['0x{0:X}'.format(condense(*pix)) for pix in im.getdata()])
             varname = fname.split('.')[0].split("/")[-1].upper()
 
-            fc.write("const uint8_t {0}_WIDTH = {1};\n".format(varname,im.size[0]))
-            fc.write("const uint8_t {0}_HEIGHT = {1};\n".format(varname,im.size[1]))
             fc.write("const uint8_t {0}_DATA[{1}] = {{{2}}};\n".format(varname,im.size[0]*im.size[1],pixels))
             fc.write("\n")
 
             fh.write("#define {0}_IMAGE {0}_WIDTH,{0}_HEIGHT,(uint8_t*){0}_DATA\n".format(varname))
-            fh.write("extern const uint8_t {0}_WIDTH;\n".format(varname))
-            fh.write("extern const uint8_t {0}_HEIGHT;\n".format(varname))
+            fh.write("#define {0}_WIDTH ({1})\n".format(varname,im.size[0]))
+            fh.write("#define {0}_HEIGHT ({1})\n".format(varname,im.size[1]))
             fh.write("extern const uint8_t {0}_DATA[{1}];\n".format(varname,im.size[0]*im.size[1]))
             fh.write("\n")
