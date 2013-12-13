5a1c79c8d29091f3c3a227c69408da8f20e34e08
Eric Van Albert <ericva1992@gmail.com>
2013-12-12 23:42:30 -0500

    rewrote menus.c
    it's better i promise

--- menus.h
+++ menus.h
@@ -1,11 +1,6 @@
 #include <stdint.h>
 
 typedef uint8_t btn_t;
-typedef enum {
-    MENU_SELF,
-    MENU_MAIN,
-    MENU_SPLASH
-} menu_state_t;
 
 #define COLOR_RED 0xE0
 #define COLOR_ORANGE 0xF4
@@ -20,7 +15,3 @@ typedef enum {
 // The main function to render the current menu 
 void render_menu();
 
-// The main settings menu
-menu_state_t menu_main(btn_t);
-// The splash screen
-menu_state_t menu_splash(btn_t);
